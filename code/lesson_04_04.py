5d9343bcfdb1b59859973b78b41e8f071f402ae9
Roy Bayot <roybayot@gmail.com>
2020-01-03 18:30:02 +0000

    Add solution 4.4

--- code/lesson_04_04.py
+++ code/lesson_04_04.py
@@ -1,3 +1,22 @@
 
-def solution(N, A):
-    pass
+def solution(A):
+    elems_of_A = set(A)
+    list_elems_of_A = list(elems_of_A)
+    list_elems_of_A.sort()
+    min_elem = list_elems_of_A[0]
+    max_elem = list_elems_of_A[-1]
+
+    elems_needed = set(range(min_elem, max_elem + 1))
+
+    elems_missing = elems_needed.difference(elems_of_A)
+
+    if len(elems_missing) == 0:
+        return 1
+    else:
+        elems_missing = list(elems_missing)
+        elems_missing.sort()
+
+        if elems_missing[0] < 1:
+            return 1
+        else:
+            return elems_missing[0]

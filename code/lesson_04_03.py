0c1a9c40c8b2a2a41814d4d90c9ca8eca04fe72e
Roy Bayot <roybayot@gmail.com>
2020-01-03 18:05:24 +0000

    added solution for lesson 4.3

--- code/lesson_04_03.py
+++ code/lesson_04_03.py
@@ -4,20 +4,14 @@ MaxCounters
 
 
 def solution(N, A):
-    nums_gt_eq_N = [i for i in A if i >= N]
-    nums_lt_N = [i for i in A if i < N]
+    bins = [0] * N
+    max_count = 0
 
-    num_gt_N = len(nums_gt_eq_N)
-    counter = [num_gt_N] * N
+    for i in A:
+        if i == N + 1:
+            bins = [max_count] * N
+        if 1 <= i and i <= N:
+            bins[i - 1] = bins[i - 1] + 1
+        max_count = max(bins)
 
-    for i in range(1, N + 1):
-        count = [1 for ii in nums_lt_N]
-        count = sum(count)
-        counter[i - 1] = counter[i - 1] + count
-
-    return counter
-
-
-"""
-NOT THE RIGHT ONE
-"""
+    return bins

76afe5b573a309c3691fef5fb7d0807a5d447e3d
Roy Bayot <roybayot@gmail.com>
2020-01-05 23:14:53 +0000

    added new solution to lesson 3.3

--- code/lesson_03_03.py
+++ code/lesson_03_03.py
@@ -7,3 +7,16 @@ def solution(A):
     splits = list(range(1, len(A)))
     diff_of_sums = [abs(sum(A[0:i]) - sum(A[i:])) for i in splits]
     return min(diff_of_sums)
+
+
+def solution2(A):
+    left_sum = 0
+    right_sum = sum(A)
+
+    abs_diff = []
+    for i in range(1, len(A)):
+        left_sum = A[i]
+        right_sum = right_sum - A[i]
+
+        abs_diff.append(abs(left_sum - right_sum))
+    return min(abs_diff)

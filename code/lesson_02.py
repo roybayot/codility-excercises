41fc63376dd52254229567a89410b6102f36cf38
Roy Bayot <roybayot@gmail.com>
2020-01-06 01:21:34 +0000

    Added other solutions

--- code/lesson_02.py
+++ code/lesson_02.py
@@ -7,3 +7,33 @@ def solution(A):
         if accum % 2 == 1:
             num_without_pair = elem
     return num_without_pair
+
+
+def solution2(A):
+    unpaired = [A[0]]
+
+    for each_elem in A[1:]:
+        matched = False
+        num_unpaired = len(unpaired)
+        i = 0
+        while i < num_unpaired and not matched:
+            if each_elem == unpaired[i]:
+                unpaired.pop(i)
+                matched = True
+            else:
+                i = i + 1
+        if not matched:
+            unpaired.append(each_elem)
+
+    return unpaired[0]
+
+
+def solution3(A):
+    A.sort()
+    A = A + [A[-1]]
+    for x in range(0, len(A), 2):
+        a = A[x]
+        b = A[x + 1]
+        if a != b:
+            return a
+    return A[-1]

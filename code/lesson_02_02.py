24ec138a8b217f9cb8ed9a64242cbbf0a05a67a6
Roy Bayot <roybayot@gmail.com>
2020-01-06 15:42:52 +0000

    Added a new solution

--- code/lesson_02_02.py
+++ code/lesson_02_02.py
@@ -1,3 +1,12 @@
 def solution(A, K):
     rotated = A[-K:] + A[:-K]
     return rotated
+
+
+def solution2(A, K):
+    len_A = len(A)
+    rotated = [0] * len_A
+    for i in range(len_A):
+        rotated[(i + K) % len_A] = A[i]
+
+    return rotated

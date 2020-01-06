f8ee649db5091a03632e2692b06ba0c07a1cdd37
Roy Bayot <roybayot@gmail.com>
2020-01-06 19:06:26 +0000

    add more complete solution to finding permutation

--- code/lesson_04_01.py
+++ code/lesson_04_01.py
@@ -3,6 +3,52 @@ PermCheck
 """
 
 
+def counting(A, m):
+    """
+    A gives the list.
+    m is the number of integers in the list from zero
+    """
+
+    n = len(A)
+    count = [0] * (m + 1)
+    for k in range(n):
+        count[A[k]] += 1
+    return count
+
+
+def solution2(A):
+    # write your code in Python 3.6
+    A.sort()
+
+    if len(A) == 0:
+        return 0
+    elif len(A) == 1 and A[0] != 1:
+        return 0
+    else:
+        count = counting(A, A[-1])
+        if sum(count[1:]) == len(count) - 1:
+            return 1
+        else:
+            return 0
+
+# solution 2. if you have [1000000000], it terminates
+
+
+def solution3(A):
+    A.sort()
+    if A[0] == 1 and A[-1] == len(A):
+        count = counting(A, A[-1])
+        if sum(count[1:]) == len(count) - 1:
+            for x in count[i:]:
+                if x != 1:
+                    return 0
+            return 1
+        else:
+            return 0
+    else:
+        return 0
+
+
 def count(A, item):
     one_count = [1 for a in A if a == item]
     return sum(one_count)

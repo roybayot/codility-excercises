271648b9f7cd1d8668770f2414ff50e70815bd63
Roy Bayot <roybayot@gmail.com>
2020-01-21 19:44:19 +0000

    add part of solution 5.1

--- code/lesson_05_01.py
+++ code/lesson_05_01.py
@@ -5,5 +5,33 @@ def solution(A):
     return total
 
 
+def prefix_sum(A):
+    len_a = len(A)
+    pref_sum = [0] * (len_a + 1)
+
+    for i in range(1, len_a + 1):
+        pref_sum[i] = pref_sum[i - 1] + A[i - 1]
+
+    return pref_sum
+
+
+def suffix_sum(A):
+    len_a = len(A)
+    suf_sum = [0] * (len_a + 1)
+    for i in range(len(A) - 1, -1, -1):
+        suf_sum[i] = A[i] + suf_sum[i + 1]
+    return suf_sum
+
+
 def solution2(A):
-    total_cars_going_west = sums(A)
+    # write your code in Python 3.6
+    pref_sum_A = prefix_sum(A)
+    total = pref_sum_A[-1]
+
+    accum = 0
+    for i in range(len(A)):
+        if A[i] == 0:
+            accum = accum + total - pref_sum_A[i + 1]
+        if accum > 1000000: # condition i did not check
+            return -1
+    return accum
